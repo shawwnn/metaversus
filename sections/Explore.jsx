4f01344e6fda3784db1f947360b83a8b76ef8aea
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-03 09:47:47 +0800

    Explore: Carousel framer motion

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -25,7 +25,7 @@ const Explore = () => {
         <TitleText title={<>Choose the world you want
         <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
 
-        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70px] gap-5">
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
             <ExploreCard 
               key={world.id}
