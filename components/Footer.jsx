e26b085314402ee053556a82739b3abfaf0c93f7
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-05 10:56:43 +0800

    Footer II

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from "framer-motion";
+import { socials } from "../constants";
 import styles from "../styles";
 import { footerVariants } from "../utils/motion";
 
@@ -23,7 +24,24 @@ const Footer = () => (
 
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
+          <p className="font-normal text-[14px] text-white opacity-50">Copyright © 2021 - 2022 Metaversus. All rights reserved.</p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img 
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
       </div>
+
+
     </div>
   </motion.footer>
 );
